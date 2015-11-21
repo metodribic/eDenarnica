3b2354ea4b9b4125b3c89bd6331726e263e377ed
metodribic <metod.ribic@gmail.com>
2015-11-21 15:59:42 +0100

    Added left_menu.html

---
+++ app/scripts/controllers/index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/**
+ * Controller for editin user settings
+ */
+angular.module('eDenarnicaApp')
+	.controller('IndexController', ['$scope', '$location','$rootScope', function($scope, $location, $rootScope) {
+
+		// TODO: GET USER
+
+		$rootScope.loggedUser = null;
+
+		
+		/* Objekt uporabnik */
+		$scope.user = {
+			name: 'Metod',
+			surname: 'Ribič',
+			username: 'metod',
+			email: 'metod.ribic@gmail.com',
+			balance: 15.346,
+			savings: 350
+		};
+		//$scope.register = false;
+		$scope.register = $scope.user;
+
+		$scope.registerUser = function(){
+			$scope.register = true;
+		};
+
+		$scope.registerCancel = function(){
+			$scope.register = false;
+		};
+
+		$scope.login = function() {
+			$rootScope.loggedUser = $scope.user;
+			$location.path('/');
+			console.log($rootScope.loggedUser);
+		};
+  	}]);
